d76538d37fe5ee58cbed14d57197aefa60f88b72
KubArthur <94832589+KubArthur@users.noreply.github.com>
2022-03-30 15:04:55 +0200

    Add files via upload

---
+++ formOffre.php
@@ -0,0 +1,93 @@
+<div class="margin">
+<h2> Création/modification d'une offre de stage </h2>
+<div class="separation"></div>
+<h3>Veuillez renseigner les informations de l'entreprise : </h3>
+
+
+<form>
+  <div class="group">
+                <label> Nom de l'entreprise* </label>
+                <input type="text"/>
+            </div>
+    <div class="form">
+            
+            <div class="group">
+                <label> Code Postal* </label>
+                <input type="text" />
+            </div>
+            <div class="group">
+                <label> ville* </label>
+                <select >
+                <option value="test">--Sélectionner une ville-- </option>
+                </select>
+            </div>
+            
+            <div class="group">
+                <label> Adresse mail du contact </label>
+                <input type="email"/>
+            </div>
+    </div>
+        <div class="Des2"> 
+            <div class="group20">
+               <button> Mettre à jour les données dans entreprise </button>
+            </div> 
+  </div>
+
+
+<div class="separation"></div>
+
+<h3> Veuillez renseigner les informations sur l'offre : </h3>
+
+
+            <div class="group">
+                <label> Intitulé de l'offre* </label>
+                <input type="text" />
+            </div>
+
+    <div class="form">
+            <div class="group" id="test">
+                <label> Début du stage* </label>
+                <input type="date" />
+            </div>
+            <div class="group">
+                <label> Durée du stage* </label>
+                <input type="number"/>
+            </div>
+            <div class="group">
+                <label> Compétence principale* </label>
+                <select  >
+                <option value="test"> --Sélectionner une majeur-- </option>
+                </select>
+            </div>
+            <div class="group">
+                <label> Compétence secondaire </label>
+                <select  >
+                <option value="test"> --Sélectionner une mineur-- </option>
+                </select>
+            </div>
+            <div class="group">
+                <label> Montant gratification </label>
+                <input type="number"/>
+            </div>
+            <div class="group">
+                <label> Nombre de place </label>
+                <input type="number"/>
+            </div>       
+    </div>
+
+            <div class="group">
+                <label> Détail de l'offre* </label>
+                <textarea placeholder="Saisissez le détail de l'offre ici..." rows="10">
+                </textarea>
+            </div>
+
+       <div class="Des">   
+          <div class="group20">
+              <button>Enregistrer</button>
+          </div>
+          <div class="group20">
+              <button>Annuler</button>
+          </div>
+      </div>
+</form>
+</div>
