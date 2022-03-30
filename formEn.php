6a5066f8e0cf96e96a12dfdaccd048b1b391f0c0
KubArthur <94832589+KubArthur@users.noreply.github.com>
2022-03-30 15:21:23 +0200

    Add files via upload

---
+++ formEn.php
@@ -0,0 +1,82 @@
+<div class="margin">
+<h2> Création/modification d'une entreprise </h2>
+<div class="separation"></div>
+<h3>Veuillez renseigner les information sur l'entreprise : </h3>
+
+<form>
+    <div class="form">
+    <div class="group">
+       <label> Banière de l'entreprise</label>
+       <img src="" alt="Banière de l'entreprise" border="1" />
+       <input type="file" max="50" /> <br>
+    </div>
+    <div class="group">
+       <label>Logo de l'entreprise</label>
+       <img src="" alt="Logo de l'entreprise" border="1" />
+       <input type="file"/>
+    </div>
+    </div>
+
+            <div class="group">
+                <label> Nom de l'entreprise* </label>
+                <input type="text" />
+            </div>
+            <div class="group">
+                <label> Présentation de l'entreprise* </label>
+                <textarea placeholder="Saisissez le détail de l'offre ici..." rows="10">
+                </textarea>
+            </div>
+     
+      <div class="form">  
+            <div class="group">
+                <label> Code Postal* </label>
+                <input type="text" />
+            </div>
+            <div class="group">
+                <label> Ville* </label>
+                <select >
+                <option value="test"> --Sélectionner une ville-- </option>
+                <option value="test"> nope </option>
+                </select>
+            </div>
+            <div class="group">
+                <label> Adresse* </label>
+                <input type="text" />
+            </div>    
+      </div>
+
+        <div class="Des"> 
+            <div class="group20">
+               <button>Rajouter</button>
+           </div>
+            <div class="group20">
+               <button>Annuler</button>
+            </div>    
+        </div>
+   
+
+</form>
+
+<div class="separation"></div>
+
+<div class="form">
+            <div class="group">
+                <label> All localités </label>
+                <select multiple>
+                <option value="test">test</option>
+                </select>
+            </div>
+            <div class="group">
+                <label> All email </label>
+                <select multiple>
+                <option value="test">test</option>
+                </select>
+            </div>
+</div>
+            <div class="form2">
+            <div class="group">
+                <label> Nombre de stagiaire </label> 
+                <input type="text" />
+            </div>
+</div>
+        
